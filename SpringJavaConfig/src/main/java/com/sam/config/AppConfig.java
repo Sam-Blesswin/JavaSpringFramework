036528a33e9b6dc22ebd93f393f0a70877e537c0
Sam <samblesswinedu@gmail.com>
2024-02-08 15:13:58 -0700

    Modified AppConfig with more annotation

--- SpringJavaConfig/src/main/java/com/sam/config/AppConfig.java
+++ SpringJavaConfig/src/main/java/com/sam/config/AppConfig.java
@@ -3,21 +3,32 @@ package com.sam.config;
 import com.sam.Coder;
 import com.sam.Computer;
 import com.sam.Desktop;
+import com.sam.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     //@Bean(name = "Beast")
     @Bean
+    @Primary
     //@Scope("prototype") //be default its singleton
     public Desktop desktop(){
         return new Desktop();
     }
 
     @Bean
-    public Coder coder(Computer computer){
+    //@Scope("prototype") //be default its singleton
+    public Laptop laptop(){
+        return new Laptop();
+    }
+
+    @Bean
+    public Coder coder (Computer computer) //(@Qualifier("laptop") Computer computer)
+    {
         Coder coder =   new Coder();
         coder.setAge(24);
         coder.setComputer(computer);
